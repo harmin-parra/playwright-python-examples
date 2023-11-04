ac3bb4abb5b9ae61df1513495bd84634ec33f03c
Harmin Parra Rueda <harmin.rueda@gmail.com>
2023-11-05 00:08:17 +0100

    add location tests

---
+++ tests/2_advanced_examples/location_test.py
@@ -0,0 +1,35 @@
+import pytest
+import time
+from urllib.parse import urlparse
+from playwright.sync_api import Page, expect
+
+
+@pytest.fixture(autouse=True)
+def goto(page: Page):
+    page.goto("https://example.cypress.io/commands/location")
+    yield
+    time.sleep(0)  # If you want to add a pause at the end of each test.
+
+
+def test_1(page: Page):
+    """ get the current URL hash """
+    url = page.url
+    res = urlparse(url)
+    assert res.fragment == ''
+
+
+def test_2(page: Page):
+    """ get window.location """
+    url = page.url
+    res = urlparse(url)
+    assert res.fragment == ''
+    assert url == "https://example.cypress.io/commands/location"
+    assert res.hostname == "example.cypress.io"
+    assert res.path == "/commands/location"
+    assert res.port is None
+    assert res.scheme == "https"
+
+
+def test_3(page: Page):
+    """ get the current URL """
+    assert page.url == "https://example.cypress.io/commands/location"
