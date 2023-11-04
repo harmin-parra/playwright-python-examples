7d213ded551d3fb2f71b03c28d1733dca1e93ac2
Harmin Parra Rueda <harmin.rueda@gmail.com>
2023-11-05 00:15:05 +0100

    refactoring

--- tests/1_getting_started/todo_test.py
+++ tests/1_getting_started/todo_test.py
@@ -7,22 +7,24 @@ import time
 def goto(page: Page):
     page.goto("https://example.cypress.io/todo")
     yield
-    time.sleep(1)  # If you want to add a pause at the end of each test.
+    time.sleep(0)  # If you want to add a pause at the end of each test.
 
 
 def test_1(page: Page):
     """ can add new todo items """
-    expect(page.locator(".todo-list li")).to_have_count(2)
-    expect(page.locator(".todo-list li").first).to_have_text("Pay electric bill")
-    expect(page.locator(".todo-list li").last).to_have_text("Walk the dog")
+    tasks = page.locator(".todo-list li")
+    expect(tasks).to_have_count(2)
+    expect(tasks.first).to_have_text("Pay electric bill")
+    expect(tasks.last).to_have_text("Walk the dog")
 
 
 def test_2(page: Page):
     """ can check off an item as completed """
     page.locator('[data-test="new-todo"]').fill("Feed the cat")
     page.locator('[data-test="new-todo"]').press("Enter")
-    expect(page.locator(".todo-list li")).to_have_count(3)
-    expect(page.locator(".todo-list li").last).to_have_text("Feed the cat")
+    tasks = page.locator(".todo-list li")
+    expect(tasks).to_have_count(3)
+    expect(tasks.last).to_have_text("Feed the cat")
 
 
 @pytest.fixture
@@ -34,19 +36,22 @@ def item_checked(page: Page):
 def test_3(page: Page, item_checked):
     """ can filter for uncompleted tasks """
     page.get_by_role("link", name="Active").click()
-    expect(page.locator(".todo-list li")).to_have_count(1)
-    expect(page.locator(".todo-list li").first).to_have_text("Walk the dog")
+    tasks = page.locator(".todo-list li")
+    expect(tasks).to_have_count(1)
+    expect(tasks.first).to_have_text("Walk the dog")
 
 
 def test_4(page: Page, item_checked):
     """ can filter for completed tasks """
     page.get_by_role("link", name="Completed").click()
-    expect(page.locator(".todo-list li")).to_have_count(1)
-    expect(page.locator(".todo-list li").first).to_have_text("Pay electric bill")
+    tasks = page.locator(".todo-list li")
+    expect(tasks).to_have_count(1)
+    expect(tasks.first).to_have_text("Pay electric bill")
 
 
 def test_5(page: Page, item_checked):
     """ can delete all completed tasks """
     page.get_by_role("button", name="Clear completed").click()
-    expect(page.locator(".todo-list li")).to_have_count(1)
-    expect(page.locator(".todo-list li")).not_to_have_text("Pay electric bill")
+    tasks = page.locator(".todo-list li")
+    expect(tasks).to_have_count(1)
+    expect(tasks).not_to_have_text("Pay electric bill")
