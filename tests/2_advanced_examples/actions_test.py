af628c226d7801dd156fb8f7ddd9434a309c8a2f
Harmin Parra Rueda <harmin.rueda@gmail.com>
2023-11-05 22:10:34 +0100

    change regex

--- tests/2_advanced_examples/actions_test.py
+++ tests/2_advanced_examples/actions_test.py
@@ -187,11 +187,11 @@ def test_11(page: Page):
     select1.select_option("fr-bananas")
     expect(select1).to_have_value("fr-bananas")
 
-    select2.select_option(['fr-apples', 'fr-oranges', 'fr-bananas'])
-    expect(select2).to_have_values(['fr-apples', 'fr-oranges', 'fr-bananas'])
+    select2.select_option(["fr-apples", "fr-oranges", "fr-bananas"])
+    expect(select2).to_have_values(["fr-apples", "fr-oranges", "fr-bananas"])
     # Don't know how to verify 'fr-oranges' is part of selected options in an elegant way with expect()
-    expect(select2).to_have_values([re.compile(".*"), 'fr-oranges', re.compile(".*")])
-    assert 'fr-oranges' in select2.evaluate("elem => $(elem).val()")
+    expect(select2).to_have_values([re.compile(".*"), re.compile(".*oranges"), re.compile(".*")])
+    assert "fr-oranges" in select2.evaluate("elem => $(elem).val()")
     options = select2.evaluate(
         "elem => { "
         "    values = [];"
@@ -200,7 +200,7 @@ def test_11(page: Page):
         "    return values;"
         "}"
     )
-    assert 'fr-oranges' in options
+    assert "fr-oranges" in options
 
 
 def test_12(page: Page):
